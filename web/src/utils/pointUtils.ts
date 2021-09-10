ab388270c0d778e1def620342ec50ec022df15aa
Syaz0326 <tadakazu326@gmail.com>
2021-09-11 02:19:05 +0900

    style(pointUtils): Change indent

--- web/src/utils/pointUtils.ts
+++ web/src/utils/pointUtils.ts
@@ -3,10 +3,9 @@ import { Point } from '../types/Point';
 export const getTwoPointDistance = (p1: Point, p2: Point) =>
   Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
 
-  export const getMidPoint = (p1: Point, p2: Point): Point => {
-    return {
-      x: (p1.x + p2.x) / 2,
-      y: (p1.y + p2.y) / 2,
-    };
+export const getMidPoint = (p1: Point, p2: Point): Point => {
+  return {
+    x: (p1.x + p2.x) / 2,
+    y: (p1.y + p2.y) / 2,
   };
-
+};
