c049eaf5b3447cc3a56c5b951a77d70d23ef1744
Syaz0326 <tadakazu326@gmail.com>
2021-09-11 02:16:19 +0900

    feat(pointUtils): Add pointUtils

--- web/src/hooks/usePinchPos.ts
+++ web/src/hooks/usePinchPos.ts
@@ -1,18 +1,12 @@
 import { RefObject, useState } from 'react';
 import { Point } from '../types/Point';
+import { getMidPoint } from '../utils/pointUtils';
 import { useEventListener } from './useEventListener';
 
 export const usePinchPos = (ref: RefObject<HTMLElement | null>) => {
   const [pos, setPos] = useState<Point>({ x: 0, y: 0 });
 
-  const getMidPoint = (p1: Point, p2: Point): Point => {
-    return {
-      x: (p1.x + p2.x) / 2,
-      y: (p1.y + p2.y) / 2,
-    };
-  };
-
-  useEventListener(ref, 'touchstart', (e) => {
+  const setMidPos = (e: TouchEvent) => {
     e.preventDefault();
 
     if (e.touches.length < 2) return { x: 0, y: 0 };
@@ -20,7 +14,10 @@ export const usePinchPos = (ref: RefObject<HTMLElement | null>) => {
     const p1 = { x: e.touches[0].pageX, y: e.touches[0].pageY };
     const p2 = { x: e.touches[1].pageX, y: e.touches[1].pageY };
     setPos(getMidPoint(p1, p2));
-  });
+  };
+
+  useEventListener(ref, 'touchstart', setMidPos);
+  useEventListener(ref, 'touchmove', setMidPos);
 
   return pos;
 };
