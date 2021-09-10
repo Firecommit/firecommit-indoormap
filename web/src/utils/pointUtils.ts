78430869f22aaf8b52f4ca554e5a6395ed0f0915
Syaz0326 <tadakazu326@gmail.com>
2021-09-11 02:23:35 +0900

    feat(pointUtils): Add getScale function

--- web/src/utils/pointUtils.ts
+++ web/src/utils/pointUtils.ts
@@ -7,3 +7,8 @@ export const getMidPoint = (p1: Point, p2: Point): Point => ({
   x: (p1.x + p2.x) / 2,
   y: (p1.y + p2.y) / 2,
 });
+
+export const getScale = (p: Point, scale: number): Point => ({
+  x: p.x / scale,
+  y: p.y / scale,
+});
