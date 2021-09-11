c21f00866224efd9f48474cd4e4508baa42df6dc
Syaz0326 <tadakazu326@gmail.com>
2021-09-11 11:42:49 +0900

    feat(usePanScale): Track mouse cursor when updating scale

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import { UsePinchPosExample } from './components/usePinchPosExample';
+import { UsePanScaleExample } from './components/usePanScaleExample';
 
 export const App = () => {
   return (
-    <div className="App">
-      <UsePinchPosExample />
+    <div>
+      <UsePanScaleExample />
     </div>
   );
 };
--- web/src/components/usePanScaleExample.tsx
+++ web/src/components/usePanScaleExample.tsx
@@ -1,12 +1,61 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Grid from '../resources/grid.svg';
 import { useScale } from '../hooks/useScale';
 import { usePan } from '../hooks/usePan';
+// import { usePinchPos } from '../hooks/usePinchPos';
+import { useMousePos } from '../hooks/useMousePos';
+import { useLast } from '../hooks/useLast';
+import {
+  getPointDivision,
+  getTwoPointDiff,
+  getTwoPointSum,
+} from '../utils/pointUtils';
+import { Point } from '../types/Point';
 
 export const UsePanScaleExample = () => {
+  const [buffer, setBuffer] = useState({ x: 0, y: 0 });
   const ref = useRef<HTMLDivElement | null>(null);
-  const scale = useScale(ref);
+  // const pinchPos = usePinchPos(ref);
+  const mousePos = useMousePos(ref);
   const [offset, startMousePan, startTouchPan] = usePan();
+  const scale = useScale(ref);
+
+  const lastOffset = useLast<Point>(offset);
+  const lastScale = useLast<number>(scale);
+
+  const offsetDelta = getTwoPointDiff(offset, lastOffset);
+
+  const initialAdjustedOffset = getTwoPointSum(offset, offsetDelta);
+  const adjustedOffsetRef = useRef<Point>(initialAdjustedOffset);
+  let adjustedOffset = adjustedOffsetRef.current;
+  useEffect(() => {
+    adjustedOffsetRef.current = adjustedOffset;
+  });
+
+  if (lastScale === scale) {
+    adjustedOffset = getTwoPointSum(
+      adjustedOffsetRef.current,
+      getPointDivision(offsetDelta, scale)
+    );
+  } else {
+    // const lastPinchPos = getPointDivision(pinchPos.current, lastScale);
+    // const newPinchPos = getPointDivision(pinchPos.current, scale);
+    const lastPinchPos = getPointDivision(mousePos.current, lastScale);
+    const newPinchPos = getPointDivision(mousePos.current, scale);
+    const pinchOffset = getTwoPointDiff(lastPinchPos, newPinchPos);
+
+    adjustedOffset = getTwoPointSum(adjustedOffsetRef.current, pinchOffset);
+  }
+
+  useLayoutEffect(() => {
+    const height = ref?.current?.clientHeight ?? 0;
+    const width = ref?.current?.clientWidth ?? 0;
+
+    setBuffer({
+      x: (width - width / scale) / 2,
+      y: (height - height / scale) / 2,
+    });
+  }, [scale, setBuffer]);
 
   return (
     <div
@@ -15,25 +64,52 @@ export const UsePanScaleExample = () => {
       onMouseDown={startMousePan}
       onTouchStart={startTouchPan}
       style={{
-        height: '100vh',
+        position: 'relative',
+        height: '200vh',
       }}
     >
       <div
         style={{
-          height: '100vh',
           backgroundImage: `url(${Grid})`,
+          backgroundSize: 200,
           transform: `scale(${scale})`,
-          backgroundPosition: `${-offset.x}px ${-offset.y}px`,
+          backgroundPosition: `${-adjustedOffset.x}px ${-adjustedOffset.y}px`,
+          position: 'absolute',
+          bottom: buffer.y,
+          left: buffer.x,
+          right: buffer.x,
+          top: buffer.y,
         }}
       />
       <div
         style={{
           position: 'absolute',
-          left: 50,
-          top: 50,
+          left: 20,
+          top: 30,
+          backgroundColor: 'white',
         }}
       >
         {JSON.stringify(offset)}
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          left: 20,
+          top: 50,
+          backgroundColor: 'white',
+        }}
+      >
+        {JSON.stringify(adjustedOffset)}
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          left: 20,
+          top: 70,
+          backgroundColor: 'white',
+        }}
+      >
+        {scale}
       </div>
     </div>
   );
