7035ad086f48b8fdc5a7eb9bc455f4f89a21b7c7
Syaz0326 <tadakazu326@gmail.com>
2021-09-11 11:45:04 +0900

    feat(usePinchPos): Change return ref

--- web/src/hooks/usePinchPos.ts
+++ web/src/hooks/usePinchPos.ts
@@ -1,10 +1,10 @@
-import { RefObject, useState } from 'react';
+import { RefObject, useRef } from 'react';
 import { Point } from '../types/Point';
 import { getMidPoint } from '../utils/pointUtils';
 import { useEventListener } from './useEventListener';
 
 export const usePinchPos = (ref: RefObject<HTMLElement | null>) => {
-  const [pos, setPos] = useState<Point>({ x: 0, y: 0 });
+  const pos = useRef<Point>({ x: 0, y: 0 });
 
   const setMidPos = (e: TouchEvent) => {
     e.preventDefault();
@@ -13,7 +13,7 @@ export const usePinchPos = (ref: RefObject<HTMLElement | null>) => {
 
     const p1 = { x: e.touches[0].pageX, y: e.touches[0].pageY };
     const p2 = { x: e.touches[1].pageX, y: e.touches[1].pageY };
-    setPos(getMidPoint(p1, p2));
+    pos.current = getMidPoint(p1, p2);
   };
 
   useEventListener(ref, 'touchstart', setMidPos);
